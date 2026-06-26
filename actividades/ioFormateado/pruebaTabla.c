3aadc7ba9a46226c4a42563cdc9ad4c0f14bc315
Fran Pacheco <franpacheco.cpp@gmail.com>
2026-06-26 09:54:20 -0300

    terminé el capítulo 3 de la primera sección 'I/O Formateado'

--- actividades/ioFormateado/pruebaTabla.c
+++ actividades/ioFormateado/pruebaTabla.c
@@ -7,5 +7,10 @@ int main(void){
   printf("|\tA\t|\tB\t|\tC\t|\n");
   printf("|%10.4f\t|\t%-8.2f|\t%-8.2f|\n", a, b, c);
 
+  printf("---------------------------\n");
+
+  float x = 3.4342f;
+  printf("%8.1f\n%-10.6f\n%8.3f\n%-6.0f\n", x, x, x, x);
+
   return 0;
 }
